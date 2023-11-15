113d8011f50f8cdb2fb86e1a4caf84a9b0dcd8e7
judaocha <52212225+judaocha@users.noreply.github.com>
2023-11-15 08:17:39 -0500

    Add files via upload

---
+++ js/sweetAlert.js
@@ -0,0 +1,8 @@
+Swal.fire({
+    title: 'Importante',
+    text: 'No dejar ningun cuadro de respuestas en blanco.',
+    imageUrl: '../images/Logo_Bikehouse_Verde_Vineta_eea50f69-f579-4834-9f5d-0b82c835c457_255x.webp',
+    imageWidth: 210,
+    imageHeight: 70,
+    imageAlt: 'Custom image',
+  })
